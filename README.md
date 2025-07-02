210829b33bd263960b7db624f2d9d3bab786d3db
salamelek <samuel.sluga@gmail.com>
2025-07-02 18:52:31 +0200

    Updated the main requirements of the website. I decided that I want it to be a 'shared personal space', except for the CV page.

--- README.md
+++ README.md
@@ -3,11 +3,12 @@ My new and improved personal website
 
 
 ## Requirements
-1) Nice looking
+1) Personal
 2) Modular & scalable
 3) Easy to maintain
 4) Well updated
 5) Dynamic(?)
+6) Good looking
 
 ## What will I use?
 Probably the usual Php + html + css + js
--- views/cv.php
+++ views/cv.php
@@ -1,1 +1,17 @@
 <?php
+$page_title = "Curriculum vitae";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php include "modules/head.php"; ?>
+<body>
+    <p>
+        <i>As you can probably see, this is the only page on this website that has a professional look.
+        This is to please the potential employer, even though I firmly belive that a website's worth is purely determined by it's content.</i>
+    </p>
+    <h1>
+        Curriculum vitae
+    </h1>
+</body>
+</html>
